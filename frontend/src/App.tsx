d7dc727192464cbeb0055009edbf912c60067dbc
Rakesh <278962698+rakesh-iim@users.noreply.github.com>
2026-05-23 12:15:19 +0530

    Uncomment ProtectedRoute and LoginPage imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-// import { ProtectedRoute } from './components/ProtectedRoute';
+import { ProtectedRoute } from './components/ProtectedRoute';
 import { AuthProvider } from './hooks/useAuth';
 import { Dashboard } from './pages/Dashboard';
-// import { LoginPage } from './pages/LoginPage';
+import { LoginPage } from './pages/LoginPage';
 import { MediaLibrary } from './pages/MediaLibrary';
 import { SectionEditorPage } from './pages/SectionEditorPage';
 
@@ -11,12 +11,12 @@ function App() {
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          {/* <Route path="/login" element={<LoginPage />} /> */}
-          {/* <Route element={<ProtectedRoute />}> */}
+          <Route path="/login" element={<LoginPage />} />
+          <Route element={<ProtectedRoute />}>
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/dashboard/media/library" element={<MediaLibrary />} />
             <Route path="/dashboard/*" element={<SectionEditorPage />} />
-          {/* </Route> */}
+          </Route>
           <Route path="*" element={<Navigate to="/dashboard" replace />} />
         </Routes>
       </BrowserRouter>
